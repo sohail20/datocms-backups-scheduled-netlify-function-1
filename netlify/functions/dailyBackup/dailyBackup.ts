73f6dd21c99c76715ca09a9af8b6a075969b16b8
Marcelo Finamor Vieira <marcelofinamorvieira@gmail.com>
2022-08-28 23:06:54 +0200

    Initial commit

---
+++ netlify/functions/dailyBackup/dailyBackup.ts
@@ -0,0 +1,31 @@
+import { schedule } from "@netlify/functions";
+import { buildClient } from "@datocms/cma-client-node";
+
+export const handler = schedule("@daily", async (event) => {
+  const client = buildClient({
+    apiToken: process.env.DATOCMS_FULLACCESS_TOKEN as string,
+  });
+
+  const environments = await client.environments.list();
+
+  const mainEnvironment = environments.find(
+    (environment) => environment.meta.primary
+  );
+
+  const previousUnusedDailyBackup = environments.find(
+    (environment) =>
+      environment.id.match("backup-plugin-daily") && !environment.meta.primary
+  );
+
+  if (previousUnusedDailyBackup) {
+    await client.environments.destroy(previousUnusedDailyBackup.id);
+  }
+
+  await client.environments.fork(mainEnvironment!.id, {
+    id: `backup-plugin-daily-${new Date().toISOString().split("T")[0]}`,
+  });
+
+  return {
+    statusCode: 200,
+  };
+});
